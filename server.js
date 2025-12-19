d6355649757d491ad8680653e62beb7a7b094834
Yash khunt <yashkhunt90@gmail.com>
2025-12-19 12:02:45 +0530

    adding changes

--- server.js
+++ server.js
@@ -4,33 +4,38 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require('./routes/authRoutes')
-const invoiceRoutes = require('./routes/invoiceRoutes')
-const aiRoutes = require('./routes/aiRoutes')
+const authRoutes = require('./routes/authRoutes');
+const invoiceRoutes = require('./routes/invoiceRoutes');
+const aiRoutes = require('./routes/aiRoutes');
 const emailRoutes = require("./routes/emailRoutes");
-const app = express();
 
-// Middleware to handle CORS
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const app = express();
 
 // Connect Database
 connectDB();
 
-// Middleware
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: ["https://vercel-front-ten.vercel.app/", "http://localhost:5173"], // UPDATE THIS
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
-// Routes Here
+// --- FIX: Add this Default Route ---
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api", emailRoutes);
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
